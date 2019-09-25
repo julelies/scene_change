4becdbe363b96e748da94803d6e43ece1193d3ab
julelies <43366085+julelies@users.noreply.github.com>
2019-09-25 11:58:47 +0200

    Add files via upload
    scene change works
    had to do new commit and repository cuz i did smth wrong

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+       private void Update() {
+         Rigidbody rb = GetComponent<Rigidbody>();
+         if (Input.GetKey(KeyCode.A))
+             rb.AddForce(Vector3.left);
+         if (Input.GetKey(KeyCode.D))
+             rb.AddForce(Vector3.right);
+         if (Input.GetKey(KeyCode.W))
+             rb.AddForce(Vector3.up);
+         if (Input.GetKey(KeyCode.S))
+             rb.AddForce(Vector3.down);
+ 
+     }
+     private void OnTriggerEnter(Collider other){
+         if(other.name=="Sphere")
+         { 
+             SceneManager.LoadScene("szen2", LoadSceneMode.Single);
+             }
+         Debug.Log("log " +other.name);
+     }
+}
