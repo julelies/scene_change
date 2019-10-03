79536393a13c907cc374ad05d9acb0350b9aecc1
julelies <43366085+julelies@users.noreply.github.com>
2019-10-03 12:56:59 +0200

    Add files via upload

---
+++ Script/Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller : MonoBehaviour
+{
+
+     private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+       private void Update() {
+           //mit rigidbody wird bewegt
+         Rigidbody rb = GetComponent<Rigidbody>();
+         if (Input.GetKey(KeyCode.A))
+             rb.AddForce(Vector3.left);
+         if (Input.GetKey(KeyCode.D))
+             rb.AddForce(Vector3.right);
+         if (Input.GetKey(KeyCode.W))
+             rb.AddForce(Vector3.up);
+         if (Input.GetKey(KeyCode.S))
+             rb.AddForce(Vector3.down);
+
+            //vector 3 has all axes 
+ 
+     }
+     private void OnTriggerEnter(Collider other){
+         if(other.name=="Sphere")
+         { 
+             SceneManager.LoadScene("szen2", LoadSceneMode.Single);
+             }
+                  }
+}
---
+++ Script/Scenechange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using LoadSceneMode;
+
+
+public class Scenechange : MonoBehaviour
+{ void Start()
+    {
+        // Only specifying the sceneName or sceneBuildIndex will load the Scene with the Single mode
+        SceneManager.LoadScene("szen1", LoadSceneMode.Additive);
+    }
+
+   public class LoadScene : MonoBehaviour
+{
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    // Use this for initialization
+    void Start()
+    {
+       // myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/szen2");
+        //scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 30), "Change Scene"))
+        {
+            Debug.Log("Scene2 loading: " + scenePaths[0]);
+            SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
+        }
+    }
+}
+    }
+
